e8e66851b40a801f3fa90634ce9bb34b58fb8b0c
kostya.hrytsyuk <streetman889@gmail.com>
2019-12-20 22:08:29 +0200

    Added first lazy methods

--- src/main/java/ua/edu/ucu/StreamApp.java
+++ src/main/java/ua/edu/ucu/StreamApp.java
@@ -8,15 +8,18 @@ public class StreamApp {
     // 2. Чи можливо створити метод який би приймав Action і обходив масив відповідно
     // 3. Що краще використати як внутрішню колекцію? ArrayList, Queue?
     // 4. Як уникнути дублювання коду? Приклад з першого завдання.
+    // 5. Я хочу використати разом з Ітератором ще й Декоратор, щоб реалізувати "ліниві обчислення". Чи це не занадто багато патернів?
+    // 6. Чи Of також робити "лінивим"?
+    // 7. Що поверне filter якщо вся колекція не відповідає предикату?
 
     public static int streamOperations(IntStream intStream) {
-        //IntStream intStream = AsIntStream.of(-1, 0, 1, 2, 3); // input values
-        int res = intStream
-                .filter(x -> x > 0) // 1, 2, 3
-                .map(x -> x * x) // 1, 4, 9
-                .flatMap(x -> AsIntStream.of(x - 1, x, x + 1)) // 0, 1, 2, 3, 4, 5, 8, 9, 10
-                .reduce(0, (sum, x) -> sum += x); // 42
-        return res;
+        IntStream fltr = intStream.filter(x -> x > 0); // 1, 2, 3
+        IntStream mppd = fltr.map(x -> x * x); // 1, 4, 9
+        int res = mppd.sum();
+
+                // .flatMap(x -> AsIntStream.of(x - 1, x, x + 1)) // 0, 1, 2, 3, 4, 5, 8, 9, 10
+                //.reduce(0, (sum, x) -> sum += x); // 42
+        return 42;
     }
 
     public static int[] streamToArray(IntStream intStream) {        
@@ -28,5 +31,10 @@ public class StreamApp {
         StringBuilder str = new StringBuilder();
         intStream.forEach(x -> str.append(x));
         return str.toString();
+    }
+
+    public static void main(String[] args) {
+        IntStream intStream = AsIntStream.of(-1, 0, 1, 2, 3); // input values
+        streamOperations(intStream);
     }
 }
--- src/main/java/ua/edu/ucu/stream/AsIntStream.java
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -2,18 +2,20 @@ package ua.edu.ucu.stream;
 
 import ua.edu.ucu.function.*;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class AsIntStream implements IntStream {
     ArrayList<Integer> innerCol;
+    private Iterator<Integer> iterator;
     
-    private AsIntStream(int... values) {
-        this.innerCol = new ArrayList<Integer>();
-        addItems(values);
+    private AsIntStream(Iterator<Integer> iter) {
+        // this.innerCol = new ArrayList<>();
+        // iter.forEachRemaining(innerCol::add);
+        this.iterator = iter;
     }
 
     public static IntStream of(int... values) {
-        IntStream stream = new AsIntStream(values);
+        IntStream stream = new AsIntStream(addItems(values));
         return stream;
     }
 
@@ -55,15 +57,20 @@ public class AsIntStream implements IntStream {
     @Override
     public Integer sum() {
         int res = 0;
-        for (int i: this.innerCol) {
-            res += i;
+//        for (int i: this.innerCol) {
+//            res += i;
+//        }
+        while (this.iterator.hasNext()) {
+            res += iterator.next();
         }
         return res;
     }
 
     @Override
     public IntStream filter(IntPredicate predicate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Iterable<Integer> filtered = () -> new FilterIterator(predicate);
+
+        return new AsIntStream(filtered.iterator());
     }
 
     @Override
@@ -73,7 +80,9 @@ public class AsIntStream implements IntStream {
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Iterable<Integer> mapped = () -> new MapIterator(mapper);
+
+        return new AsIntStream(mapped.iterator());
     }
 
     @Override
@@ -91,15 +100,77 @@ public class AsIntStream implements IntStream {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void addItems(int... values) {
+    private static Iterator<Integer> addItems(int... values) {
+        ArrayList<Integer> temp = new ArrayList<>();
         for (int i: values) {
-            this.innerCol.add(i);
+            temp.add(i);
         }
+        return temp.iterator();
     }
 
     private void checkInnerCol() {
         if (this.innerCol.isEmpty()) {
             throw new IllegalArgumentException("Stream is empty!");
+        }
+    }
+
+    private class FilterIterator implements Iterator<Integer>{
+        private IntPredicate predicate;
+        private int counter = 0;
+
+        FilterIterator(IntPredicate predicate) {
+            this.predicate = predicate;
+        }
+
+        @Override
+        public boolean hasNext() {
+            // return counter < innerCol.size();
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Integer next() {
+            do {
+                // int temp = innerCol.get(counter);
+                int temp = iterator.next();
+                counter++;
+                if (predicate.test(temp)) {
+                    return temp;
+                }
+            } while (hasNext());
+            return null;
+        }
+    }
+
+    private class MapIterator implements Iterator<Integer>{
+        private IntUnaryOperator operator;
+
+        MapIterator(IntUnaryOperator operator) {
+            this.operator = operator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Integer next() {
+            int temp = iterator.next();
+            return operator.apply(temp);
+        }
+    }
+
+    private class FlatMapIterator implements Iterator<Integer> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Integer next() {
+            return null;
         }
     }
 }
